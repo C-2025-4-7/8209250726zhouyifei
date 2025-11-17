41b01cfea0f4c2c495a2742ae312ead9cbc9a9fe
a13172593707 <2442901705@qq.com>
2025-11-17 19:14:56 +0800

    Add files via upload

---
+++ 1.3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "char length:" << sizeof(char) << endl;
+	cout << "int length:" << sizeof(int) << endl;
+	cout << "float length:" << sizeof(float) << endl;
+	cout << "double length:" << sizeof(double) << endl;
+	cout << "long length:" << sizeof(long) << endl;
+	cout << "long long length:" << sizeof(long long) << endl;
+	cout << "bool length:" << sizeof(bool) << endl;
+
+
+
+
+	return 0;
+}
