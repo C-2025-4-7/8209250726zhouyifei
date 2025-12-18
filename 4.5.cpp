1207c51ba85dabb008ab878cc77a3da556fb1efb
a13172593707 <2442901705@qq.com>
2025-12-18 19:37:33 +0800

    Create 4.5.cpp

---
+++ 4.5.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int indexOf(const char s1[], const char s2[]) {
+    int len1 = strlen(s1);
+    int len2 = strlen(s2);
+    
+    if (len1 == 0 || len1 > len2) {
+        return -1;
+    }
+    
+    for (int i = 0; i <= len2 - len1; ++i) {
+        bool match = true;
+        for (int j = 0; j < len1; ++j) {
+            if (s2[i + j] != s1[j]) {
+                match = false;
+                break;
+            }
+        }
+        if (match) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    char s1[100], s2[100];
+    cout << "请输入子串s1：";
+    cin >> s1;
+    cout << "请输入字符串s2：";
+    cin >> s2;
+    
+    int pos = indexOf(s1, s2);
+    if (pos != -1) {
+        cout << "s1是s2的子串，起始下标为：" << pos << endl;
+    } else {
+        cout << "s1不是s2的子串" << endl;
+    }
+    
+    return 0;
+}
