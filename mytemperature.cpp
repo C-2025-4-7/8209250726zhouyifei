1855370d98217b8f9770715d365809aec48cecb7
a13172593707 <2442901705@qq.com>
2025-12-18 19:26:33 +0800

    Create mytemperature.cpp

---
+++ mytemperature.cpp
@@ -0,0 +1,9 @@
+#include "mytemperature.h"
+
+double celsius_to_fah(double cel) {
+    return cel * 9.0 / 5.0 + 32.0;
+}
+
+double fahrenheit_to_cels(double fah) {
+    return (fah - 32.0) * 5.0 / 9.0;
+}
